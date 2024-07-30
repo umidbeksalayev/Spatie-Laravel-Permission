c2f7612bef3c43692789499455e124c2fd2eb0d2
umidbeksalayev <yoldoshevich406@gmail.com>
2024-07-30 10:22:57 +0500

    Created AdminSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Database\Seeders;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,9 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       $this->call(RoleSeeder::class);
+       $this->call(AdminSeeder::class);
     }
 }
