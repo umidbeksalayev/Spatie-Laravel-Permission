203f28d776f71784dc4847cb8f11d0a47119b020
umidbeksalayev <yoldoshevich406@gmail.com>
2024-07-30 14:52:21 +0500

    Create Roles and Permissions Pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,9 +25,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->middleware(['auth', 'role:admin'])->name('admin.index');
+Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->group(function () {
+Route::get('/',[IndexController::class,'index'])->name('index');
+Route::resource('roles', RoleController::class);
+Route::resource('permissions', PermissionController::class);
+});
 
 
 Route::middleware('auth')->group(function () {
